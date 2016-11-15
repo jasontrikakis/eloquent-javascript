f17569295c02949b88254f5c694031440ac5fee8
Jason Trikakis <jason_trikakis@mac.com>
2016-11-15 09:46:12 -0800

    Add files via upload

---
+++ ex-2.js
@@ -0,0 +1,25 @@
+data = require('./ancestry-data');
+
+ancestry = JSON.parse(data);
+
+function average(array) {
+  function plus(a, b) {
+    return a + b;
+  };
+  return array.reduce(plus) / array.length;
+}
+
+var byName = {};
+ancestry.forEach(function(person) {
+  byName[person.name] = person;
+});
+
+var ageDiff = [];
+
+ancestry.forEach(function(person) {
+  if (byName[person.mother]) {
+    ageDiff.push(Math.abs((byName[person.mother].born - person.born)));
+  };
+});
+
+console.log(average(ageDiff));
